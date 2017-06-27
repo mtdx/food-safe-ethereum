f2f1b485dce6cb1530ed08e9abe712cb4d117c1b
mtdx <ms2asdk@gmail.com>
2017-06-27 13:10:03 -0700

    add create contract

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -3,7 +3,9 @@ import { default as Web3} from 'web3';
 import { default as contract } from 'truffle-contract'
 var accounts;
 var account;
-
+var foodSafeABI;
+var foodSafeContract;
+var foodSafeCode;
 window.App = {
   start: function() {
     var self = this;
@@ -12,7 +14,6 @@ window.App = {
         alert("There was an error fetching your accounts.");
         return;
       }
-
       if (accs.length == 0) {
         alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
         return;
@@ -20,7 +21,26 @@ window.App = {
 
       accounts = accs;
       account = accounts[0];
+      web3.eth.defaultAccount= account;
+      var foodSafeSource= "pragma solidity ^0.4.6; contract FoodSafe {    struct Location{        string Name;        uint LocationId;        uint PreviousLocationId;        uint Timestamp;        string Secret;    }        mapping(uint => Location) Trail;    uint8 TrailCount=0;    function AddNewLocation(uint LocationId, string Name, string Secret)   {        Location memory newLocation;        newLocation.Name = Name;        newLocation.LocationId= LocationId;        newLocation.Secret= Secret;        newLocation.Timestamp = now;        if(TrailCount!=0)        {            newLocation.PreviousLocationId= Trail[TrailCount].LocationId;        }        Trail[TrailCount] = newLocation;        TrailCount++;    }    function GetTrailCount() returns(uint8){        return TrailCount;    }    function GetLocation(uint8 TrailNo) returns (string,uint,uint,uint,string)    {        return (Trail[TrailNo].Name, Trail[TrailNo].LocationId, Trail[TrailNo].PreviousLocationId, Trail[TrailNo].Timestamp,Trail[TrailNo].Secret);    }}";
+      web3.eth.compile.solidity(foodSafeSource, function(error, foodSafeCompiled){
+        console.log(error);
+        console.log(foodSafeCompiled);
+        foodSafeABI = foodSafeCompiled['<stdin>:FoodSafe'].info.abiDefinition;
+        foodSafeContract = web3.eth.contract(foodSafeABI);
+        foodSafeCode = foodSafeCompiled['<stdin>:FoodSafe'].code;
+
+      });
     });
+  },
+    createContract: function()
+  {
+    foodSafeContract.new("", {from:account, data: foodSafeCode, gas: 3000000}, function (error, deployedContract){
+      if(deployedContract.address)
+      {
+        document.getElementById("contractAddress").value=deployedContract.address;
+      }
+    })
   },
 };
 
